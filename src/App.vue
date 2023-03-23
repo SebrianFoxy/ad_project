530293faf4f6defafb2d49b2be5f28fbbca944d4
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-03-23 23:33:19 +0300

    Создание всех страниц + Подключение экранов к роутеру

--- src/App.vue
+++ src/App.vue
@@ -3,58 +3,38 @@
     <v-navigation-drawer app v-model="drawer">
       <v-list-item>
         <v-list-item-content>
-              <v-list-item-title class="title">
-                КИПУ
-              </v-list-item-title>
-              <v-list-item-subtitle>
-                Учебный проект
-              </v-list-item-subtitle>
+          <v-list-item-title class="title">
+            КИПУ
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Учебный проект
+          </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-divider></v-divider>
       <v-list dense>
-      <v-list-item
-      v-for="link in links"
-      :key="link.title"
-      >
-      <template v-slot:prepend>
-      <v-icon :icon="link.icon"></v-icon>
-      </template>
-      <v-list-item-title>{{ link.title }}</v-list-item-title>
-      </v-list-item>
+        <v-list-item v-for="link in links" :key="link.title">
+          <template v-slot:prepend>
+            <v-icon :icon="link.icon"></v-icon>
+          </template>
+          <v-list-item-title>{{ link.title }}</v-list-item-title>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn
-        v-for="link in links"
-        :key="link.title"
-        ><v-icon
-        start
-        :icon="link.icon"
-        ></v-icon>
-        {{ link.title }}
+        <v-btn text v-for="link in links" :key="link.title">
+          <v-icon start :icon="link.icon"></v-icon>
+          {{ link.title }}
         </v-btn>
-
-        <v-btn>
-          <v-icon start icon="mdi-cake-variant"></v-icon>
-          Link One
-        </v-btn>
-        <v-btn>Link Two</v-btn>
-        <v-btn>Link Three</v-btn>
       </v-toolbar-items>
     </v-app-bar>
-    <v-content>
+    <v-main>
       <router-view></router-view>
-    </v-content>
+    </v-main>
   </v-app>
-  <v-main>
-  <router-view></router-view>
-  </v-main>
-
-
 </template>
 
 

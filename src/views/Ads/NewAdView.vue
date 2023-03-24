38438a6cb6348fc44223929c3cfd5e307e398a4c
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-03-24 14:52:57 +0300

    Верстка формы

--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -1,7 +1,16 @@
 <template>
     <v-container>
-        <v-row justify="center">
-            <h1>Home</h1>
+        <v-row>
+            <v-col cols="8" offset="2">
+                <h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
+                <v-form v-model="valid" ref="form" validation>
+                    <v-text-field name="title" label="Ad Title" type="text" v-model="title"
+                        :rules="[v => !!v || 'Title is required']">
+                    </v-text-field>
+                    <v-textarea name="description" label="Ad Description" type="text" v-model="description"
+                        :rules="[v => !!v || 'Description is required']" class="mb-3"></v-textarea>
+                </v-form>
+            </v-col>
         </v-row>
     </v-container>
 </template>
@@ -9,7 +18,11 @@
 export default {
     data() {
         return {
-        }
-    }
-}
+            valid: false,
+            title: "",
+            description: "",
+            promo: false,
+        };
+    },
+};
 </script>

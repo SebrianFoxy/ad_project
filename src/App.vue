ee8f0b190e6b0ce55b85495264d6d512a56c8eff
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-03-30 20:20:27 +0300

    LogOut

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
       </v-list-item>
       <v-divider></v-divider>
       <v-list dense>
-        <v-list-item v-for="link in links" :key="link.title" :to="link.url">
+        <v-list-item v-for="link in links" :key="link.title" :to="link.url" >
           <template v-slot:prepend>
             <v-icon :icon="link.icon"></v-icon>
           </template>
@@ -30,9 +30,23 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn v-for="link in links" :key="link.title" :to="link.url">
-          <v-icon start :icon="link.icon"></v-icon>
-          {{ link.title }}
+        <v-btn
+        v-for="link in links"
+        :key="link.title"
+        :to="link.url"
+        >
+        <v-icon
+        start
+        :icon="link.icon"
+        ></v-icon>
+        {{ link.title }}
+        </v-btn>
+        <v-btn @click="onLogout" v-if="isUserLoggedIn">
+        <v-icon
+        start
+        icon="mdi-exit-to-app"
+        ></v-icon>
+        Logout
         </v-btn>
       </v-toolbar-items>
     </v-app-bar>
@@ -90,7 +104,12 @@ export default {
   methods: {
     closeError() {
       this.$store.dispatch('clearError')
+    },
+    onLogout() {
+      this.$store.dispatch('logoutUser')
+      this.$router.push("/")
     }
+
   }
 
 }
--- src/store/user.js
+++ src/store/user.js
@@ -65,8 +65,10 @@ export default {
 				throw 'Упс... Ошибка логина или пароля'
 			})
 		}
-	}
-
+	},
+		logoutUser({commit}) {
+			commit('setUser', null)
+		}
 },
 getters: {
 	user(state) {

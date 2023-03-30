977fa52cb78e1ea404ce361782d83ae722c230b9
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-03-30 20:43:46 +0300

    Рефакторинг метода createAd

--- src/store/ads.js
+++ src/store/ads.js
@@ -39,9 +39,9 @@ export default {
 		}
 	},
 	actions: {
-		createAd({commit},payload){
+		createAd({commit, getters},payload){
 			payload.id = Math.random()
-			commit('createAd', payload)
+			payload.userId = getters.user != null ? getters.user.id : '1'
 		}
 	},
     getters: {
@@ -57,9 +57,34 @@ export default {
 			return state.ads
 	},
 	adById(state) {
-	return id => {
-	return state.ads.find(ad => ad.id == id)
+		return id => {
+			return state.ads.find(ad => ad.id == id)
 	}
-}
-    }
+	}		
+    },
+	async createAd({commit, getters},payload){
+		payload.id = Math.random()
+		payload.userId = getters.user != null ? getters.user.id : '1'
+		commit('clearError')
+		commit('setLoading', true)
+		//Заглушка запроса
+		let isRequestOk = true
+		let promise = new Promise(function(resolve) {
+		setTimeout(() => resolve('Done')
+		, 3000);
+		});
+		if (isRequestOk) {
+		await promise.then(()=> {
+		commit('createAd', payload)
+		commit('setLoading', false)
+		})
+		} else {
+		await promise.then(()=> {
+		commit('setLoading', false)
+		commit('setError', 'Ошибка создания объявления')
+		throw 'Упс... Ошибка создания объявления'
+		})
+		}
+
+		}
 }
